f6cfd65aaf49ed39516a465a9e24b03e5f877ed3
Ashish Jain <ashish.jain@intelligrape.com>
2015-06-27 11:06:39 +0530

    Screen 4 Controller added.

---
+++ GitDemo/Screen4/Screen4ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Screen4ViewController.h
+//  GitDemo
+//
+//  Created by Ashish Jain on 27/06/15.
+//  Copyright (c) 2015 TTN Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Screen4ViewController : UIViewController
+
+@end
---
+++ GitDemo/Screen4/Screen4ViewController.m
@@ -0,0 +1,37 @@
+//
+//  Screen4ViewController.m
+//  GitDemo
+//
+//  Created by Ashish Jain on 27/06/15.
+//  Copyright (c) 2015 TTN Digital. All rights reserved.
+//
+
+#import "Screen4ViewController.h"
+
+@interface Screen4ViewController ()
+
+@end
+
+@implementation Screen4ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
