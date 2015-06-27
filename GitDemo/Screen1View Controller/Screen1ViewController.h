7bdaa4e112674dacc4630d1a93d4e41659c03343
piyushb <piyushb@intelligrape.com>
2015-06-27 11:10:11 +0530

    Added screen1 view controller

---
+++ GitDemo/Screen1View Controller/Screen1ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Screen1ViewController.h
+//  GitDemo
+//
+//  Created by Intelligrape on 27/06/15.
+//  Copyright (c) 2015 TTN Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Screen1ViewController : UIViewController
+
+@end
