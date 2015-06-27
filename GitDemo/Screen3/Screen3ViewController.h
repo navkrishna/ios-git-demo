eb8d11cd4b46f07778a44719ff9ddc85e2199da4
Harish Kumar Yadav <harishy@intelligrape.com>
2015-06-27 11:17:27 +0530

    Screen 3 created.

---
+++ GitDemo/Screen3/Screen3ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Screen3ViewController.h
+//  GitDemo
+//
+//  Created by Harry on 27/06/15.
+//  Copyright (c) 2015 TTN Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Screen3ViewController : UIViewController
+
+@end
---
+++ GitDemo/Screen3/Screen3ViewController.m
@@ -0,0 +1,37 @@
+//
+//  Screen3ViewController.m
+//  GitDemo
+//
+//  Created by Harry on 27/06/15.
+//  Copyright (c) 2015 TTN Digital. All rights reserved.
+//
+
+#import "Screen3ViewController.h"
+
+@interface Screen3ViewController ()
+
+@end
+
+@implementation Screen3ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
